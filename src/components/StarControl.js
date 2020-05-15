6a50ccb4a9e70cbdb8094c997e916019aba99bd8
Prestwick97 <prestwick97@gmail.com>
2020-05-15 12:01:13 -0700

    Study details of React structure for first half the day

---
+++ src/components/StarControl.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StarBirth from './StarBirth';
+import LowMassControl from './LowMasses/LowMassControl';
+import HighMassControl from './HighMasses/HighMassControl';
