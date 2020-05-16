40902963280d5d056c9e4063c38a6a64edb5ece0
Prestwick97 <prestwick97@gmail.com>
2020-05-15 18:57:35 -0700

    create form in starControl

--- src/components/StarControl.js
+++ src/components/StarControl.js
@@ -3,3 +3,30 @@ import PropTypes from 'prop-types';
 import StarBirth from './StarBirth';
 import LowMassControl from './LowMassControl';
 import HighMassControl from './HighMassControl';
+
+class StarControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      starBirthHomePage: true,
+      highMassControl:false,
+      lowMassControl:false
+    };
+  }
+
+  handleClick = () => {
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <div>
+          <form>
+              <input type='number' name='mass' placeholder='solar mass (kg)'/>
+            <button onClick={this.handleClick}>{"Create your star"}></button>
+          </form>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
