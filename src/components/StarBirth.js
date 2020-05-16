c4b15bbbd85402f0907d94501780a756859fac86
Prestwick97 <prestwick97@gmail.com>
2020-05-16 14:41:19 -0700

    create static home page

--- src/components/StarBirth.js
+++ src/components/StarBirth.js
@@ -1,13 +1,18 @@
-// import React from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
+// import Star from './Star';
 
-// function StarBirth(props) {
-// <React.Fragment>
-//         <div class='createStar'>
-//           <form>
-//               <input type='number' name='mass' placeholder='solar mass (kg)'/>
-//             <button onClick={this.handleClick}>{"Create your star"}</button>
-//           </form>
-//         </div>
-// </React.Fragment>
-// }
+function StarBirth(props) {
+  return(
+    <React.Fragment>
+            <div class='createStar'>
+              <form>
+                  <input type='number' name='mass' placeholder='solar mass (kg)'/>
+                <button>{"Create your star"}</button>
+              </form>
+            </div>
+    </React.Fragment>
+  );
+}
+
+export default StarBirth;
--- src/components/StarControl.js
+++ src/components/StarControl.js
@@ -4,33 +4,57 @@ import StarBirth from './StarBirth';
 import LowMassControl from './LowMassControl';
 import HighMassControl from './HighMassControl';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
+import Footer from './Footer';
 
 class StarControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      starBirthHomePage: true,
-      highMassControl:false,
-      lowMassControl:false
+      starMass: null
     };
   }
 
-  handleClick = () => {
+  StarChange = () => {
+
   }
 
   render() {
     return(
-         <Router>
-         <Header />
-         <Switch>
-           <Route exact path="/" component={Home} />
-           <Route path="/memorycontrol" component={MemoryControl} />
-           <Route path="/signin" component={Signin} />
-           <Route path="/contact" component={Contact} />
-         </Switch>
-         <Footer />
-       </Router>
-    )
+      <StarBirth />
+    );
   }
 }
 export default StarControl;
+  
+
+
+
+
+
+
+// constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     starBirthHomePage: true,
+  //     highMassControl:false,
+  //     lowMassControl:false
+  //   };
+  // }
+  
+  // handleClick = () => {
+  // }
+  
+  // render() {
+  //   return(
+  //        <Router>
+  //        <Header />
+  //        <Switch>
+  //          <Route exact path="/" component={StarBirth} />
+  //          <Route path="/LowMassControl" component={LowMassControl} />
+  //          <Route path="/HighMassControl" component={HighMassControl} />
+  //        </Switch>
+  //        <Footer />
+  //      </Router>
+  //   );
+  // }
